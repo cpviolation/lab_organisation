91d71a6be34f5f7d01952a8f41cb333e4077333d
cpviolation <martinelli.maurizio@gmail.com>
2024-03-26 09:37:47 +0100

    bugfix

--- macros/update_attendance_db.py
+++ macros/update_attendance_db.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser('Create Attendance Database Options')
 parser.add_argument('--db_name', type=str, default='data/attendance.db', help='Database name')
 parser.add_argument('--students_db_name', type=str, default='data/students.db', help='Database name')
 parser.add_argument('--cohort', type=str, help='Cohort name')
-parser.add_argument('--json_dates', type=str, help='Input JSON with course dates', required=True)
+parser.add_argument('--json_dates', type=str, help='Input JSON with course dates')
 parser.add_argument('--json_attendance', type=str, help='Input JSON with attendance results', nargs='+')
 parser.add_argument('--dryrun', action='store_true', help='Dry run')
 args = parser.parse_args()
@@ -17,6 +17,8 @@ args = parser.parse_args()
 def main():
     # create the attendance database if not existing
     if not os.path.exists(args.db_name):
+        if args.json_dates is None:
+            raise ValueError('Please provide a JSON file with course dates')
         create_attendance_db(args.json_dates, args.db_name)
     
     # update the attendance database
